f5a815ead3f1c6228fabc8b315fbc86f57ee033f
Naratna <59068049+Naratna@users.noreply.github.com>
2020-11-28 14:56:43 -0600

    Add project files.

---
+++ NecronomiconBot/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using System;
+using System.Configuration;
+using System.Threading.Tasks;
+
+namespace NecronomiconBot
+{
+    class Program
+    {
+
+        private System.Configuration.Configuration config = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+
+        static void Main(string[] args)
+            => new Program().MainAsync(args).GetAwaiter().GetResult();
+
+        public Task MainAsync(string[] args)
+        {
+            if (args.Length == 1)
+            {
+                config.AppSettings.Settings["Token"].Value = args[0];
+                config.Save();
+            }
+            string token = config.AppSettings.Settings["Token"].Value;
+
+            return Task.CompletedTask;
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+
+    }
+}
