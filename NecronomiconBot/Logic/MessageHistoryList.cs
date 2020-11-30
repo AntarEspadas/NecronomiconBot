729ab0e474e4a3d7d958272fc13490cf74c58cee
Naratna <59068049+Naratna@users.noreply.github.com>
2020-11-29 19:34:43 -0600

    Added MessageHistoryList class in order to track data about deletion status and deletion time of messages

---
+++ NecronomiconBot/Logic/MessageHistoryList.cs
@@ -0,0 +1,13 @@
+﻿using Discord;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NecronomiconBot.Logic
+{
+    class MessageHistoryList : LinkedList<IMessage>
+    {
+        public bool isDeleted { get; set; } = false;
+        public DateTimeOffset? DeletedTimestamp { get; set; } = null;
+    }
+}
