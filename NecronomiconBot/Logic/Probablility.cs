c53a034e423c37f86a638efa266a2b374a5b0986
Naratna <59068049+Naratna@users.noreply.github.com>
2020-12-04 00:28:14 -0600

    Classes for easier dice rolls and converting strings to dice rolls

---
+++ NecronomiconBot/Logic/Probablility.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace NecronomiconBot.Logic
+{
+    [TypeConverter(typeof(TypeConverterProbablillityConverter))]
+    class Probablility
+    {
+        public float Percentage { get { return percentage; } set => SetPercentage(value); }
+        private float percentage;
+        private static Random random = new Random();
+
+        public Probablility(float percentage)
+        {
+            Percentage = percentage;
+        }
+        private void SetPercentage(float value)
+        {
+            if (percentage < 0 || percentage > 100)
+            {
+                throw new ArgumentException();
+            }
+            percentage = value;
+        }
+        public bool Roll()
+        {
+            return random.NextDouble() < (Percentage / 100);
+        }
+    }
+}
