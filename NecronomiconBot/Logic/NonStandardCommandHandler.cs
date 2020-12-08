7cf90b6eed5a8b953f1a623d86a1baaa22b4210e
Naratna <59068049+Naratna@users.noreply.github.com>
2020-12-07 23:27:49 -0600

    Added logic for OnMention command

--- NecronomiconBot/Logic/NonStandardCommandHandler.cs
+++ NecronomiconBot/Logic/NonStandardCommandHandler.cs
@@ -1,4 +1,5 @@
-﻿using Discord.Commands;
+﻿using Discord;
+using Discord.Commands;
 using Discord.Commands.Builders;
 using Discord.WebSocket;
 using NecronomiconBot.Modules;
@@ -23,12 +24,31 @@ namespace NecronomiconBot.Logic
         public async Task HandleCommandAsync(SocketUserMessage message)
         {
             var context = new SocketCommandContext(_client, message);
-            if (new Random().Next(0,500) == 69)
+            int argPos = 0;
+            if (IsOnlyMention(message, _client.CurrentUser))
+            {
+                var utility = new Utility();
+                utility.SetContext(context);
+                await utility.OnMention();
+            }
+            else if (new Random().Next(0,500) == 69)
             {
                 var memes = new Memes();
                 memes.SetContext(context);
                 await memes.FakeQuote(message.Content);
             }
+        }
+
+        private bool IsOnlyMention(IMessage message, IUser user)
+        {
+            string content = message.Content;
+            if (string.IsNullOrEmpty(content) || content.Length < 4 || content[0] != '<' || content[1] != '@' || content[content.Length -1] != '>')
+                return false;
+            if (!MentionUtils.TryParseUser(content, out ulong userId))
+                return false;
+            if (userId == user.Id)
+                return true;
+            return false;
         }
     }
 }
