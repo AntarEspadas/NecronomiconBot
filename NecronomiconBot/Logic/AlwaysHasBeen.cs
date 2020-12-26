adfe2862a0df9b78ec1ec314bbb91cf9a50b12cf
Naratna <59068049+Naratna@users.noreply.github.com>
2020-12-26 05:06:53 -0600

    Changed AlwaysHasBeen to work with ImageSharp library Changed the assets folder to be divided into subfolders Added arial.ttf to assets/fonts

--- NecronomiconBot/Logic/AlwaysHasBeen.cs
+++ NecronomiconBot/Logic/AlwaysHasBeen.cs
@@ -2,51 +2,128 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
-using OpenCvSharp;
-using System.Drawing;
+using SixLabors.ImageSharp;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Drawing.Processing;
+using SixLabors.Fonts;
+using SixLabors.ImageSharp.Formats;
 
 namespace NecronomiconBot.Logic
 {
     public static class AlwaysHasBeen
     {
-        public static Stream GetImage(string astronaut1, string astronaut2, string messageContent)
+
+        private static readonly string assetsFolder = Path.Combine(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location), "assets");
+
+        public static Stream GetImage(string astronaut1, string astronaut2, string messageContent, bool debugRectangle = false)
         {
-            Mat mat = new Mat(@".\assets\wait-its-all-ohio.png");
-
-            PutCenteredText(mat, messageContent, new OpenCvSharp.Point(425, 200), HersheyFonts.HersheyComplex, 0.75, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
-            PutCenteredText(mat, messageContent, new OpenCvSharp.Point(425, 200), HersheyFonts.HersheyComplex, 0.75, new Scalar(255, 255, 255), 1, LineTypes.AntiAlias);
-
-            PutCenteredText(mat, astronaut1, new OpenCvSharp.Point(430, 245), HersheyFonts.HersheyComplex, 0.5, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
-            PutCenteredText(mat, astronaut1, new OpenCvSharp.Point(430, 245), HersheyFonts.HersheyComplex, 0.5, new Scalar(255, 255, 255), 1, LineTypes.AntiAlias);
-
-            PutCenteredText(mat, astronaut2, new OpenCvSharp.Point(755, 80), HersheyFonts.HersheyComplex, 0.75, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
-            PutCenteredText(mat, astronaut2, new OpenCvSharp.Point(755, 80), HersheyFonts.HersheyComplex, 0.75, new Scalar(255, 255, 255), 1, LineTypes.AntiAlias);
-
-            PutCenteredText(mat, "Always has been", new OpenCvSharp.Point(600, 35), HersheyFonts.HersheyComplex, 1, new Scalar(0, 0, 0), 2, LineTypes.AntiAlias);
-            PutCenteredText(mat, "Always has been", new OpenCvSharp.Point(600, 35), HersheyFonts.HersheyComplex, 1, new Scalar(255, 255, 255), 2, LineTypes.AntiAlias);
-
-
             MemoryStream stream = new MemoryStream();
-            mat.WriteToStream(stream);
+            FontCollection fonts = new FontCollection();
+            fonts.Install(Path.Combine(assetsFolder, "fonts", "arial.ttf"));
+            Font captionFont = fonts.CreateFont("arial", 8);
+            Rectangle messageContentRectangle = new Rectangle(150,50,450,165);
+            Font astronaut1Font = fonts.CreateFont("arial", 7);
+            Rectangle astronaut1Rectangle = new Rectangle(350, 225, 150, 75);
+            Font astronaut2Font = fonts.CreateFont("arial", 8);
+            Rectangle astronaut2Rectangle = new Rectangle(625,50,250,125);
+            Font alwaysHasBeenFont = fonts.CreateFont("arial", 9);
+            Rectangle alwaysHasBeenRectangle = new Rectangle(400, 0, 475, 50);
+            using (Image img = Image.Load(Path.Combine(assetsFolder, "images", "wait-its-all-ohio.png")))
+            {
+                img.Mutate(ctx => ctx.DrawBoxedText(astronaut1Font, astronaut1, Color.White, Color.Black, astronaut1Rectangle, VerticalAlignment.Center, debugRectangle));
+                img.Mutate(ctx => ctx.DrawBoxedText(astronaut2Font, astronaut2, Color.White, Color.Black, astronaut2Rectangle, VerticalAlignment.Center, debugRectangle));
+                img.Mutate(ctx => ctx.DrawBoxedText(captionFont, messageContent, Color.White, Color.Black, messageContentRectangle, VerticalAlignment.Bottom, debugRectangle));
+                img.Mutate(ctx => ctx.DrawBoxedText(alwaysHasBeenFont, "Always has been", Color.White, Color.Black, alwaysHasBeenRectangle, VerticalAlignment.Center, debugRectangle));
+                img.SaveAsPng(stream);
+            }
             stream.Position = 0;
-
-
             return stream;
         }
+        private static IImageProcessingContext DrawBoxedText(this IImageProcessingContext processingContext,
+            Font font,
+            string text,
+            Color mainColor,
+            Color outlineColor,
+            Rectangle rectangle,
+            VerticalAlignment verticalAlignment,
+            bool debugRectangle = false)
+        {
+            Size imgSize = processingContext.GetCurrentSize();
+            float targetWidth = rectangle.Width;
+            float targetHeight = rectangle.Height;
 
-        private static SizeF MeasureString(string s, Font f)
-        {
-            using (var image = new Bitmap(1,1))
-            using (var g = Graphics.FromImage(image))
+            float targetMinHeight = (int)(targetHeight * 0.95);
+
+            var scaledFont = font;
+            FontRectangle s = new FontRectangle(0, 0, float.MaxValue, float.MaxValue);
+
+            float scaleFactor = (scaledFont.Size / 2);
+            int trapCount = (int)scaledFont.Size * 2;
+            if (trapCount < 10)
             {
-                return g.MeasureString(s, f);
+                trapCount = 10;
             }
-        }
-        private static void PutCenteredText(Mat mat, string text, OpenCvSharp.Point org, HersheyFonts font, double fontScale, Scalar color, int thickness = 1, LineTypes lineType = LineTypes.Link8)
-        {
-            var size = MeasureString(text, SystemFonts.DefaultFont);
-            int width = (int)(size.Width * fontScale / 0.6);
-            mat.PutText(text, new OpenCvSharp.Point(org.X - width, org.Y), font, fontScale, color, thickness, lineType);
+
+            bool isTooSmall = false;
+
+            while ((s.Height > targetHeight || s.Height < targetMinHeight) && trapCount > 0)
+            {
+                if (s.Height > targetHeight)
+                {
+                    if (isTooSmall)
+                    {
+                        scaleFactor = scaleFactor / 2;
+                    }
+
+                    scaledFont = new Font(scaledFont, scaledFont.Size - scaleFactor);
+                    isTooSmall = false;
+                }
+
+                if (s.Height < targetMinHeight)
+                {
+                    if (!isTooSmall)
+                    {
+                        scaleFactor = scaleFactor / 2;
+                    }
+                    scaledFont = new Font(scaledFont, scaledFont.Size + scaleFactor);
+                    isTooSmall = true;
+                }
+                trapCount--;
+
+                s = TextMeasurer.Measure(text, new RendererOptions(scaledFont)
+                {
+                    WrappingWidth = targetWidth
+                });
+            }
+
+            var textGraphicOptions = new TextGraphicsOptions()
+            {
+                TextOptions = {
+                    HorizontalAlignment = HorizontalAlignment.Center,
+                    VerticalAlignment = verticalAlignment,
+                    WrapTextWidth = targetWidth
+                }
+            };
+            var position = new PointF(rectangle.X, 0);
+            switch (verticalAlignment)
+            {
+                case VerticalAlignment.Top:
+                    break;
+                case VerticalAlignment.Center:
+                    position.Y = rectangle.Y + rectangle.Height / 2;
+                    break;
+                case VerticalAlignment.Bottom:
+                    position.Y = rectangle.Y + rectangle.Height;
+                    break;
+                default:
+                    break;
+            }
+            if (debugRectangle)
+            {
+                PointF[] points = { new PointF(rectangle.X, rectangle.Y), new PointF(rectangle.X, rectangle.Y + rectangle.Height), new PointF(rectangle.X + rectangle.Width, rectangle.Y + rectangle.Height), new PointF(rectangle.X + rectangle.Width, rectangle.Y) };
+                processingContext = processingContext.DrawPolygon(new Pen(Color.Red, 5), points);
+            }
+            return processingContext.DrawText(textGraphicOptions, text, scaledFont, Brushes.Solid(mainColor), Pens.Solid(outlineColor, font.Size / 10), position);
         }
     }
 }
